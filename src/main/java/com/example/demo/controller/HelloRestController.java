669f334ed615091591681e6bd24949f710a369be
ManaliParab12 <manaliparab10@gmail.com>
2021-01-13 19:40:46 +0530

    Used GET request Method passed name as query parameter

--- src/main/java/com/example/demo/controller/HelloRestController.java
+++ src/main/java/com/example/demo/controller/HelloRestController.java
@@ -1,13 +1,26 @@
 package com.example.demo.controller;
 
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/hello")
 public class HelloRestController {
+
+    // curl localhost:8080/hello -w "\n"
     @RequestMapping(value = {"", "/", "/home"})
     public String sayHello() {
         return "Hello from BridgeLabz";
     }    
+
+    // curl localhost:8080/hello/query?name=Manali -w "\n"
+    // curl -X GET "http://localhost:8080/hello/query/?fName=Manali&lName=Parab" -w "\n"
+    @RequestMapping(value = {"/query"}, method = RequestMethod.GET)
+    public String sayHello(@RequestParam(value = "fName") String fName, @RequestParam(value = "lName") String lName) {
+        return "Hello " + fName + " " + lName + " !";
+    }   
+
+
 }
